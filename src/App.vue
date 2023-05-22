587ce8681561065252befa19d2ad84f07b395b3c
medoum <mohamedoum2430@gmail.com>
2023-05-22 09:39:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Modal :header="header" :text="text" theme="ggdhh" />
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data(){
+    return{
+      title : 'Working late on My first vue App :)',
+      header : "Sign up for the Giveaway",
+      text : "Mohamed is becoming more motivated about programming"
+    }
+  },
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
